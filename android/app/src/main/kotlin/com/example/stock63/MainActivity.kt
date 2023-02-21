955e74cd04697edb0a8984f8263971e053abd2cd
wjdanfu <wjdanfu@gmail.com>
2023-02-21 21:38:07 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/stock63/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stock63
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined),label: '홈'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined),label: '샵')
+        ],
+
+      ),
+    );
+  }
+}
+
