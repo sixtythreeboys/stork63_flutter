782c3f879ffcf9e4638ed13196d3b0bed670f4fc
wjdanfu <wjdanfu@gmail.com>
2023-02-24 12:56:21 +0900

    create switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,13 +14,77 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      body: MultSwitch(),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home_outlined),label: '홈'),
           BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined),label: '샵')
         ],
 
+      ),
+    );
+  }
+}
+
+
+class MultSwitch extends StatefulWidget {
+  const MultSwitch({Key? key}) : super(key: key);
+
+  @override
+  State<MultSwitch> createState() => _MultSwitchState();
+}
+
+class _MultSwitchState extends State<MultSwitch> {
+  bool val1 = true;
+  bool val2 = false;
+
+  onChangeFunction1(bool newValue1){
+    setState(() {
+      val1 = newValue1;
+    });
+  }
+
+  onChangeFunction2(bool newValue2){
+    setState(() {
+      val2 = newValue2;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        customSwitch('연속 하락/상승', val1, onChangeFunction1),
+        customSwitch('조건 2', val2, onChangeFunction2)
+
+      ],
+    );
+  }
+
+  Widget customSwitch(String text, bool val, Function onChangeMethod){
+    return Padding(
+      padding: const EdgeInsets.only(top: 22.0, left: 16.0, right: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(text, style: const TextStyle(
+            fontSize: 20.0,
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w600,
+            color: Colors.black
+          )),
+          Spacer(),
+          CupertinoSwitch(
+            trackColor: Colors.grey,
+            activeColor: Colors.blue,
+            value: val,
+            onChanged: (newValue) {
+              onChangeMethod(newValue);
+            }
+            )
+        ],
       ),
     );
   }
