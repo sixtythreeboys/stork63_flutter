6b964574777e6d1be95ab3bcb3a51d0d9d0b0f23
wjdanfu <wjdanfu@gmail.com>
2023-02-26 17:30:25 +0900

    visiable widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,25 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: MultSwitch(),
+      body: Column(
+        children: [
+          const MultiSwitch(),
+          ElevatedButton(
+              onPressed: (){
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (c)
+                    => const Result()
+                    )
+                );
+              },
+              child: Text("조회"))
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home_outlined),label: '홈'),
@@ -27,20 +41,24 @@ class MyApp extends StatelessWidget {
 }
 
 
-class MultSwitch extends StatefulWidget {
-  const MultSwitch({Key? key}) : super(key: key);
+class MultiSwitch extends StatefulWidget {
+
+  const MultiSwitch({Key? key}) : super(key: key);
 
   @override
-  State<MultSwitch> createState() => _MultSwitchState();
+  State<MultiSwitch> createState() => _MultiSwitchState();
 }
 
-class _MultSwitchState extends State<MultSwitch> {
+class _MultiSwitchState extends State<MultiSwitch> {
+
   bool val1 = true;
   bool val2 = false;
 
   onChangeFunction1(bool newValue1){
     setState(() {
+      print(val1);
       val1 = newValue1;
+
     });
   }
 
@@ -56,7 +74,14 @@ class _MultSwitchState extends State<MultSwitch> {
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        customSwitch('연속 하락/상승', val1, onChangeFunction1),
+        Column(
+          children: [
+            customSwitch('연속 하락/상승', val1, onChangeFunction1),
+            Option1(val1: val1)
+          ],
+
+        ),
+
         customSwitch('조건 2', val2, onChangeFunction2)
 
       ],
@@ -75,7 +100,7 @@ class _MultSwitchState extends State<MultSwitch> {
             fontWeight: FontWeight.w600,
             color: Colors.black
           )),
-          Spacer(),
+          const Spacer(),
           CupertinoSwitch(
             trackColor: Colors.grey,
             activeColor: Colors.blue,
@@ -90,3 +115,44 @@ class _MultSwitchState extends State<MultSwitch> {
   }
 }
 
+
+class Result extends StatelessWidget {
+  const Result({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Text("ws"),
+      ),
+    );
+  }
+}
+
+class Option1 extends StatelessWidget {
+  Option1({Key? key, required this.val1}) : super(key: key);
+
+  bool val1;
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      visible: val1,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 22.0, left: 16.0, right: 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("선택", style: const TextStyle(
+                fontSize: 17.0,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w400,
+                color: Colors.black
+            )),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
